43c26c3c87f82ea8f6732baa5ce950d87be45e4b
ashutosh6120 <ashutosh4064@gmail.com>
2021-05-16 00:21:38 +0530

    program added

---
+++ systemCalls/program 3/pr3.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main()
+{
+    pid_t pid, mypid, myppid;
+    int i=10;
+    pid = getpid();
+    printf("before fork: process id is %d\n", pid);
+    pid = fork();
+
+    if(pid < 0)
+    {
+    perror("fork() failure\n");
+    return 1;
+    }
+
+    //Child process
+    if(pid == 0)
+    {
+        printf("This is child process\n");
+        execlp("ls", "ls",NULL);
+        printf("end of child process\n"); //is executed only if there is error in execlp call
+    }
+
+    else{ //parent process
+        wait(NULL);
+        printf("This is Parent process\n");
+    }
+    printf("i=%d\n",i);
+    return 0;
+}
